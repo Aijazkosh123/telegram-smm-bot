b1973ab87585efdc73498318a96064d20dd3ace5
Aijazkosh123 <142679544+Aijazkosh123@users.noreply.github.com>
2026-07-05 08:47:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
 
-const TOKEN = process.env.BOT_TOKEN;
-const API_URL = process.env.API_URL;
-const API_KEY = process.env.API_KEY;
+const TOKEN = "8828008114:AAH_KEYToEiQWicI6nJSHhdlBBw4Lgpg1hQ";
+const API_URL = "https://gotosmmpanel.com/api/v2";
+const API_KEY = "e2cd27de7d4a9ca720fe1991a5e9b128092bc9fd";
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
@@ -27,6 +27,7 @@ bot.onText(/\/balance/, async (msg) => {
       `💰 Balance: ${res.data.balance} ${res.data.currency}`
     );
   } catch (e) {
+    console.error(e.response?.data || e.message);
     bot.sendMessage(msg.chat.id, "❌ API Error");
   }
 });
